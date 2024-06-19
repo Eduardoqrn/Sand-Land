63019824943b00ca4bc80329561c30927b9e35cf
Eduardo Paiva <141378399+Eduardoqrn@users.noreply.github.com>
2024-06-19 00:37:53 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let wave1 = document.getElementById('wave1');
+let wave2 = document.getElementById('wave2');
+let wave3 = document.getElementById('wave3');
+let wave4 = document.getElementById('wave4');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    wave1.style.backgroundPositionX = 400 + value * 4 + 'px';
+    wave2.style.backgroundPositionX = 300 + value * -4 + 'px';
+    wave3.style.backgroundPositionX = 200 + value * 2 + 'px';
+    wave4.style.backgroundPositionX = 100 + value * -2 + 'px';
+})
+
